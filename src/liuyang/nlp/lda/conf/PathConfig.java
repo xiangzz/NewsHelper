a3d00af71c1b1f904f4c937fe962efdb26c6d07b
xiangzz <xiangzhengzhe@126.com>
2015-06-03 20:29:05 +0800

    拷贝
    拷贝

---
+++ src/liuyang/nlp/lda/conf/PathConfig.java
@@ -0,0 +1,10 @@
+package liuyang.nlp.lda.conf;
+
+public class PathConfig {
+
+	//public static String ldaDocsPath = "data/LdaOriginalDocs/";
+	public static String ldaDocsPath = "data/news/";
+	
+	public static String LdaResultsPath = "data/LdaResults/";
+
+}
